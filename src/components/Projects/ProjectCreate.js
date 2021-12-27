13edaa702c3595e15a7dcb81665010dc0cc0d0b7
AceDev28 <Trinhtheanh789@Gmail.com>
2021-12-27 19:13:10 +0700

    Chọn trạng thái của thêm project -> select option

--- src/components/Projects/ProjectCreate.js
+++ src/components/Projects/ProjectCreate.js
@@ -10,8 +10,8 @@ class ProjectCreate extends Component {
       tongThoiGianLam: "",
       idProjectManager: "",
       idTrangThai: "",
+      trangThai: [],
     };
-    this.some = this.some.bind(this);
     this.changeTenHandler = this.changeTenHandler.bind(this);
     this.changeNgayBatDauHandler = this.changeNgayBatDauHandler.bind(this);
     this.changeTongThoiGianLamHandler =
@@ -21,6 +21,14 @@ class ProjectCreate extends Component {
     this.changeIdTrangThaiHandler = this.changeIdTrangThaiHandler.bind(this);
   }
 
+  componentDidMount() {
+    axios
+      .get("http://localhost:8080/api/vi/trangThaiProject")
+      .then((response) => {
+        this.setState({ trangThai: response.data });
+      });
+  }
+  //
   saveProject = (e) => {
     // e.preventDefault();
     let project = {
@@ -31,14 +39,18 @@ class ProjectCreate extends Component {
       idTrangThai: this.state.idTrangThai,
     };
     console.log("project => " + JSON.stringify(project));
+    let config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
 
-    axios.post(`http://localhost:8080/api/vi/project`, project).then((res) => {
-      this.props.history.push("/api/vi/task");
-    });
+    axios
+      .post(`http://localhost:8080/api/vi/project`, project, config)
+      .then((res) => {});
   };
-  some = () => {
-    this.props.history.push("/");
-  };
+
   changeTenHandler = (event) => {
     this.setState({ tenProject: event.target.value });
   };
@@ -60,6 +72,7 @@ class ProjectCreate extends Component {
   };
 
   render() {
+    // console.log(this.state.trangThai);
     const styleTable = {
       border: "1px solid black",
       margin: "0 auto",
@@ -73,68 +86,80 @@ class ProjectCreate extends Component {
         <h1>Project</h1>
         <form>
           <table style={styleTable}>
-            <tr>
-              <td>Tên :</td>
-              <td>
-                <input
-                  type="text"
-                  name="ten"
-                  value={this.state.tenProject}
-                  onChange={this.changeTenHandler}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Ngày bắt đầu :</td>
-              <td>
-                <input
-                  type="date"
-                  name="ngayBatDau"
-                  value={this.state.ngayBatDau}
-                  onChange={this.changeNgayBatDauHandler}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Tổng thời gian làm :</td>
-              <td>
-                <input
-                  type="text"
-                  name="tongThoiGian"
-                  value={this.state.tongThoiGianLam}
-                  onChange={this.changeTongThoiGianLamHandler}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Project manager :</td>
-              <td>
-                <input
-                  type="text"
-                  name="projectManager"
-                  value={this.state.idProjectManager}
-                  onChange={this.changeIdProjectManagerHandler}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Trạng thái :</td>
-              <td>
-                <input
-                  type="text"
-                  name="trangThai"
-                  value={this.state.idTrangThai}
-                  onChange={this.changeIdTrangThaiHandler}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td colSpan="2">
-                <button type="submit" onClick={this.saveProject}>
-                  Nhập
-                </button>
-              </td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>Tên :</td>
+                <td>
+                  <input
+                    type="text"
+                    name="ten"
+                    value={this.state.tenProject}
+                    onChange={this.changeTenHandler}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Ngày bắt đầu :</td>
+                <td>
+                  <input
+                    type="date"
+                    name="ngayBatDau"
+                    value={this.state.ngayBatDau}
+                    onChange={this.changeNgayBatDauHandler}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Tổng thời gian làm :</td>
+                <td>
+                  <input
+                    type="text"
+                    name="tongThoiGian"
+                    value={this.state.tongThoiGianLam}
+                    onChange={this.changeTongThoiGianLamHandler}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Project manager :</td>
+                <td>
+                  <input
+                    type="text"
+                    name="projectManager"
+                    value={this.state.idProjectManager}
+                    onChange={this.changeIdProjectManagerHandler}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>Trạng thái :</td>
+                <td>
+                  {/* <input
+                    type="text"
+                    name="trangThai"
+                    value={this.state.idTrangThai}
+                    onChange={this.changeIdTrangThaiHandler}
+                  /> */}
+                  <select
+                    value={this.state.id}
+                    onChange={this.changeIdTrangThaiHandler}
+                  >
+                    {this.state.trangThai.map((trangThai) => (
+                      <option value={trangThai.id} key={trangThai.id}>
+                        {trangThai.tenTrangThai}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2">
+                  <button type="submit" onClick={this.saveProject}>
+                    Nhập
+                  </button>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </form>
       </div>
